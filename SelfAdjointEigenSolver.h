434ff6182e4f17b295657f19ce4f2923b93fa09a
sunxin9213 <13591012592@163.com>
2022-10-30 20:33:52 +0800

    abd

--- SelfAdjointEigenSolver.h
+++ SelfAdjointEigenSolver.h
@@ -2,11 +2,7 @@
 // for linear algebra.
 //
 // Copyright (C) 2008-2010 Gael Guennebaud <gael.guennebaud@inria.fr>
-// Copyright (C) 2010 Jitse Niesen <jitse@maths.leeds.ac.uk>
-//
-// This Source Code Form is subject to the terms of the Mozilla
-// Public License v. 2.0. If a copy of the MPL was not distributed
-// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Cop
 
 #ifndef EIGEN_SELFADJOINTEIGENSOLVER_H
 #define EIGEN_SELFADJOINTEIGENSOLVER_H
